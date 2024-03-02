ae6907f4304bf31e55cc3dafcdc3f52f6e13d7ff
Faizgeeky <nandoliyafaiz429@gmail.com>
2024-03-02 08:36:43 +0530

    landing removed

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -18,7 +18,7 @@ export default function Index() {
     return (
         <Preloader>
             <LandingLayout>
-                <Landing />
+                {/* <Landing /> */}
                 <Aiassist />
                 {/* <HeroSection />
                 
